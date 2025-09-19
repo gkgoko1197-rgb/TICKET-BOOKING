f3e10d9c6be46dbb44ff3ceacf02bc0d977f21f6
Dinesh <dineshgopi0505@gmail.com>
2025-09-19 12:37:40 +0000

    remove footer (_for element footer_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "StayFinder",
@@ -32,7 +31,6 @@ export default function RootLayout({
       >
         <Header />
         <main className="flex-grow">{children}</main>
-        <Footer />
         <Toaster />
       </body>
     </html>
